695cfe291200b971232463a1645ed23adb3346ab
Cesar <cesare_ar@hotmail.com>
2021-06-21 20:56:27 -0500

    agregar clases y listas con foreach

---
+++ assets/js/dom.js
@@ -0,0 +1,22 @@
+ let subtitle = document.querySelector("#subtitle");
+let parrafo = document.querySelector("#parrafo");
+console.log(subtitle);
+console.log(subtitle.textContent);
+subtitle.textContent ="Nuevo valor";
+
+
+subtitle.classList.add("red");
+
+subtitle.classList.remove("red");
+
+const animales=["Perro","Gato","Mapache","Pez","Leopardo","Raton","Zorro"];
+const list = document.querySelector("#list");
+
+animales.push("Elefante");
+
+animales.forEach(function(animal){
+    let item=document.createElement("li");
+    item.textContent=animal;
+    list.appendChild(item);
+});
+
